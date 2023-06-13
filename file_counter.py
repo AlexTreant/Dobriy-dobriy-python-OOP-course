e8f2c5bfeac4aa3e8bf15d036a5943ae6ab3f0bb
AlexTreant <9605299@gmail.com>
2023-06-13 08:13:39 +0500

    1

--- file_counter.py
+++
@@ -1,6 +0,0 @@
-from zipfile import ZipFile
-
-with ZipFile('workbook.zip') as zp_file:
-    info = zp_file.infolist()
-    answer = len([x for i, x in enumerate(info) if info[i].is_dir() == False])
-    zp_file.printdir()
