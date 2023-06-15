e06f9c369a4f7f781f7cd8ee35e4aa3195a04098
AlexTreant <9605299@gmail.com>
2023-06-15 13:36:47 +0500

    studie plot

---
+++ my_first_vizualise.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import json
+from matplotlib.ticker import AutoMinorLocator, FixedLocator
+
+def get_data_plot():
+    with open('my_studie_file.json', encoding='utf-8') as fl:
+        data = json.load(fl)
+        return data
+
+def data_plot():
+    data = get_data_plot()
+    x_list = []
+    y_list = []
+    for k, v in data.items():
+        x_list.append(k)
+        y = sum(v.values())
+        y_list.append(y)
+    print(x_list)
+    print(y_list)
+    return x_list, y_list
+
+plt.figure(figsize=(15, 40))
+plt.title("График моей учёбы", fontsize=14, fontweight='bold', color='red')
+plt.xlabel('Календарь', fontsize=12, fontweight='bold', color='black')
+plt.ylabel('Время учёбы', fontsize=12, fontweight='bold', color='black')
+
+plt.barh(*data_plot(), label='Моё обучение')
+
+plt.legend()
+plt.grid()
+plt.savefig('test_saved_figure.png')
+plt.show()
--- studie_time.py
+++ studie_time.py
@@ -9,10 +9,11 @@ with open('studie_time.txt', 'r', encoding='utf-8') as file, open('my_studie_fil
         dt = new_s[0][2:]
 #        dt = datetime.strptime(new_s[0], '__%Y-%m-%d,%A') Дата не подходит для ключа словаря в json
         d_time = {}
-        for t in new_s[1:-1]:
-            h, m = t.split(',')[-2].split(':')
-            all_t = int(h) * 60 + int(m)
-            subj = t.split(',')[-1]
-            d_time[subj] = d_time.get(subj, 0) + all_t
+        for t in new_s[:-1]:
+            if t[0].isdigit():
+                h, m = t.split(',')[2].split(':')
+                all_t = int(h) * 60 + int(m)
+                subj = t.split(',')[-1]
+                d_time[subj] = d_time.get(subj, 0) + all_t
         d[dt] = d_time
     json.dump(d, w_file)
